94e9002732bd08838cfcf91bc51fdc88f9e29e2d
Nate Carroll <nathan.carroll@fjorgedigital.com>
2024-02-28 14:05:28 -0600

    clean up some code comments, and update the README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GoldyGopher
+
+## Overview
+
+This is a web application that allows the user to input upper and lower bounds, and then outputs all numbers within those bounds--except multiples of 3 are replaced with "Goldy," multiples of 7 are replaced with "Gopher," and multiples of both 7 and 3 (aka multiples of 21) are replaced with "Goldy Gopher."
+
+## How to Run
+
+The application is written in vanilla Javascript, so there are no dependencies and you can run it in any browser with JS enabled. Clone the repository, and open the **index.html** file in a web browser of your choice.
+
+## To Do
+
+The web UI is using default styles for input, button, and paragraph elements. Next steps would be to source a styles file into **index.html** and give those elements a visually appealing look.
--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    //Log callback to confirm the file is loaded
     console.log('main.js loaded');
 
     // Display the numbers and Goldy/Gopher replacements on the DOM
     function start(){
-        let lowerBound = parseInt(document.getElementById("lowerBound").value) || 0;
-        let upperBound = parseInt(document.getElementById("upperBound").value) || 100;
-        console.log('click start');
-        console.log(lowerBound);
-        console.log(upperBound)
+        let lowerBound = parseInt(document.getElementById("lowerBound").value) || 0; // Default to 0 if input is unparsable for some reason
+        let upperBound = parseInt(document.getElementById("upperBound").value) || 100; // Default to 100
+
         for (let i=lowerBound; i<=upperBound; i++){
             let output;
-            if (i % 21 === 0){
+            if (i % 21 === 0){ // Divisible by both 7 and 3
                 output = "Goldy Gopher";
             } else if (i % 7 === 0){
                 output = "Gopher";
